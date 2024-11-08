ee6cb09b70ce9c9d4878d934887bc0396b9f1931
kailashEswar <132008559+kailashEswar@users.noreply.github.com>
2024-11-09 01:53:01 +0530

    Add files via upload

---
+++ PrefabCreator.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PrefabCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject dragonprefab;
+    [SerializeField] private Vector3 prefabOffset;
+
+    private GameObject dragon;
+    private ARTrackedImageManager aRTrackedImageManager;
+
+
+    private void OnEnable()
+    {
+        aRTrackedImageManager = gameObject.GetComponent<ARTrackedImageManager>();
+
+        aRTrackedImageManager.trackedImagesChanged += OnImageChanged;
+
+    }
+
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs obj)
+    {
+        foreach (ARTrackedImage image in obj.added)
+        {
+            dragon = Instantiate(dragonprefab, image.transform);
+            dragon.transform.position += prefabOffset;
+        }
+
+    }
+}
